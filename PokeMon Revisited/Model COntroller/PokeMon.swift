1d9661afa46ccf0d20c8ee2ce0807f672e727a5e
iyinraphael <iyinraphael@gmail.com>
2018-12-09 15:38:08 -0800

    created model and nested struct to access API structure

---
+++ PokeMon Revisited/Model COntroller/PokeMon.swift
@@ -0,0 +1,35 @@
+//
+//  PokeMon.swift
+//  PokeMon Revisited
+//
+//  Created by Iyin Raphael on 12/9/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+struct PokeMon: Decodable {
+    
+    let name: String
+    let id: Int
+    let abilities: [Abilities]
+    struct Abilities: Codable {
+        let ability: [Ability]
+        struct Ability: Codable {
+            let name: String
+        }
+    }
+    
+    let sprites: [Sprites]
+    struct Sprites: Codable {
+        let frontDefault: String
+    }
+    
+    let types: [Types]
+    struct Types: Codable {
+        let type: [SubType]
+        struct SubType: Codable {
+            let name: String
+        }
+    }
+    
+}
